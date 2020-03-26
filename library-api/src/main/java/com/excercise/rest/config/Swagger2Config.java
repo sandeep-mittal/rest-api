bf85cdceafe87ef7df9f81a3ef4496fb1092c7f7
Sandeep Mittal <Sandeep.Mittal@WEXINC.COM>
2020-03-26 03:51:43 +0000

    Initial Commit

---
+++ library-api/src/main/java/com/excercise/rest/config/Swagger2Config.java
@@ -0,0 +1,42 @@
+package com.excercise.rest.config;
+
+import com.google.common.collect.Sets;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spring.web.plugins.Docket;
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+
+                .protocols(Sets.newHashSet("http"))
+                .select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.excercise.rest.controller"))
+                .paths(PathSelectors.regex("/.*"))
+                .build()
+                .apiInfo(apiInfo());
+
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Library API")
+                .description("Library API")
+                .version("1.0.0")
+                .contact(new Contact("Sandeep Mittal", "", "write2sandeepkr@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .build();
+    }
+
+}
